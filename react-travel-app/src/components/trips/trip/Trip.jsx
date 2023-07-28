c2d069f56d2f23d3ebfddfcdf2e53c90fc0f21d2
Fatima Askerova <f.askerova@riberry.az>
2023-07-28 09:17:31 +0400

    added trips list from mock up data

--- react-travel-app/src/components/trips/trip/Trip.jsx
+++ react-travel-app/src/components/trips/trip/Trip.jsx
@@ -4,30 +4,30 @@ import "./Trip.css";
 import Button from "../../common/button/Button";
 import { useHistory } from "react-router-dom";
 
-export const Trip = () => {
+export const Trip = ({ trip }) => {
   const history = useHistory();
 
   const handleTripDisplay = () => {
-    history.push("/trip");
+    history.push(`/trip/${trip.id}`);
   };
 
   return (
     <li data-test-id="trip-card" className="trip-card">
-      <img data-test-id="trip-card-image" src={tripImg} alt="trip" />
+      <img data-test-id="trip-card-image" src={trip.image} alt="trip" />
       <div className="trip-card__content">
         <div className="trip-info">
           <h3 data-test-id="trip-card-title" className="trip-info__title">
-            Iceland
+            {trip.title}
           </h3>
           <div className="trip-info__content">
             <span
               data-test-id="trip-card-duration"
               className="trip-info__duration"
             >
-              <strong>15</strong> days
+              <strong>{trip.duration}</strong> days
             </span>
             <span data-test-id="trip-card-level" className="trip-info__level">
-              easy
+              {trip.level}
             </span>
           </div>
         </div>
@@ -37,7 +37,7 @@ export const Trip = () => {
             data-test-id="trip-card-price-value"
             className="trip-price__value"
           >
-            7000 $
+            {trip.price} $
           </strong>
         </div>
       </div>
